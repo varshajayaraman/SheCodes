7752d35d417cf55908e0fc333761c61868c5cb10
Varsha Jayaraman <varsha.jayaraman15@gmail.com>
2019-09-04 18:28:49 -0500

    Two Sum without repetitive elements

--- src/LeetCode/E1_TwoSum.java
+++ src/LeetCode/E1_TwoSum.java
@@ -1,9 +1,38 @@
 package LeetCode;
 
+import java.util.HashMap;
+
 public class E1_TwoSum{
-
-    public static void E1_TwoSum(){
+    static HashMap<Integer, Integer> hMap = new HashMap<>();
+    public static void E1_TwoSum(int[] array, int target) {
         System.out.println("In TwoSum");
+        makeHash(array);
+        int[] resArray = findTarget(array, target);
+        if (resArray[0] == -1) {
+            System.out.println("No elements are present to add up to the given Target");
+        } else {
+            System.out.println("The operands are present in the indices: " + resArray[0] + " &" + resArray[1]);
 
+        }
+    }
+    public static void makeHash(int[] array){
+        for(int i=0; i<array.length;i++){
+            hMap.put(array[i], i);
+        }
+    }
+
+    public static int[] findTarget(int[] array, int target){
+        int[] resArray = new int[2];
+        for(int i=0; i<array.length;i++){
+            int compl = target-array[i];
+            if(hMap.containsKey(compl)){
+                resArray[0] = i;
+                resArray[1] = hMap.get(compl);
+
+            }
+        }
+        resArray[0] = -1;
+        resArray[1] = -1;
+        return resArray;
     }
 }
--- src/LeetCode/Main.java
+++ src/LeetCode/Main.java
@@ -1,8 +1,24 @@
 package LeetCode;
 import LeetCode.E1_TwoSum;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args){
-        System.out.println("Hello");
-        E1_TwoSum.E1_TwoSum();
+
+        System.out.println("Two Sums");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter length of array:");
+        int length = sc.nextInt();
+        int[] array = new int[length];
+        for(int i=0;i<length;i++){
+            System.out.println("Please enter "+i+"th number:");
+            array[i] = sc.nextInt();
+        }
+        System.out.println("You have entered: "+ Arrays.toString(array));
+        System.out.println("Please enter sum target: ");
+        int target = sc.nextInt();
+        E1_TwoSum.E1_TwoSum(array, target);
     }
 }
