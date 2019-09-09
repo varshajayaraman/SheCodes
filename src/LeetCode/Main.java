30a0aec23955476a4988b6cc0815a949e5ed61d1
Varsha Jayaraman <varsha.jayaraman15@gmail.com>
2019-09-09 00:03:00 -0500

    Merge Sorted Linked List - Modified Main to hold complete LL

--- src/LeetCode/Main.java
+++ src/LeetCode/Main.java
@@ -57,13 +57,13 @@ public class Main{
 
         ListNode l1 = new ListNode(1);
         l1.next = new ListNode(2);
-        l1 = l1.next;
-        l1.next = new ListNode(4);
+        //l1 = l1.next;
+        l1.next.next = new ListNode(4);
 
         ListNode l2 = new ListNode(1);
         l2.next = new ListNode(3);
-        l2 = l2.next;
-        l2.next = new ListNode(4);
+        //l2 = l2.next;
+        l2.next.next = new ListNode(4);
 
         ListNode f = E21_MergeTwoSortedLists.mergeTwoLists(l1, l2);
         E21_MergeTwoSortedLists.printList(f);;
