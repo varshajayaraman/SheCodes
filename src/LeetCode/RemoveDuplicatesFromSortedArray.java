fd163bc234556e07c3dc3e0eabc042d15b587e00
Varsha Jayaraman <varsha.jayaraman15@gmail.com>
2019-09-22 14:49:00 -0500

    Removed unnecessary count

--- src/LeetCode/RemoveDuplicatesFromSortedArray.java
+++ src/LeetCode/RemoveDuplicatesFromSortedArray.java
@@ -9,7 +9,6 @@ public class RemoveDuplicatesFromSortedArray {
             return 1;
         }
 
-        int c=1;
         int j=1;
         for(int i=1;i<arr.length;i++){
             if(arr[i] != arr[i-1]){
